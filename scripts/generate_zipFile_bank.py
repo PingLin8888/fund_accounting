bd3bb82384440b099490f1d866c74b1fedb4269e
Ping Lin <limphoebe8@gmail.com>
2026-04-06 13:39:22 +0100

    load config file

--- scripts/generate_zipFile_bank.py
+++ scripts/generate_zipFile_bank.py
@@ -2,5 +2,15 @@ import os
 import zipfile
 import yaml
 
-# Define file paths
-input_folder_a = ""
+# __file__ the full path of the script that is currently running
+# dirname takes a full path and returns the directory part, removing the filename.
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # folder where script lives
+config_file = os.path.join(PROJECT_ROOT, "config", "config.yaml")
+
+# load file paths
+with open (config_file, "r") as f:
+    config = yaml.safe_load(f)
+
+print("config loaded: ")
+for key, value in config.items():
+    print(key, ":", value)
